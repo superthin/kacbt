b3e1f35093bf8f2a33c04544512e1f909fcf0419
Super Thin <superthin@yahoo.com>
2025-08-20 18:19:28 +0700

    Publishing new posts today 18:19:25

--- publish.sh
+++ publish.sh
@@ -6,5 +6,5 @@ sed -i 's/<meta name="generator" content="Hugo 0\.147\.9">//g' index.html
 find . -name "*.html" -exec sed -i 's/<script src="\/livereload\.js?mindelay=10\&amp;v=2\&amp;port=\&amp;path=livereload" data-no-instant defer><\/script>//g' {} \;
 cd ..
 git add .
-git commit -m "Pubush new post today $current_time"
+git commit -m "Publishing new posts today $current_time"
 git push --force --set-upstream origin main
